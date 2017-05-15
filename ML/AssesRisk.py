f7d15a599b3bb331751c293559cccb33ffed9208
Sneha <snehaa.94@gmail.com>
2017-05-15 00:10:56 -0700

    Update AssesRisk.py

--- ML/AssesRisk.py
+++ ML/AssesRisk.py
@@ -6,50 +6,29 @@ import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 
- #Load dataset
-url = "/home/poojitha/Insurance/InsData.csv"
-names = ['DMV', 'VehicleType', 'Age', 'Location', 'CreditScore','DrivingExperience','AnnualMileage','Gender','MaritalStatus','RiskCategory']
-#dataset = pandas.read_csv(url, names=names)
-# Split-out validation dataset
-#array = dataset.values
+def predictRisk(input):
+ 	#Load dataset
+	url = "/home/sneha/Documents/CMPE272/272_proj/data.csv"
+	#names = ['dmv', 'vehicle_type', 'age', 'location', 'credit_score','driving_exp','annual_mileage','gender','marital_status','risk_category']
+	names = ['DMV', 'VehicleType', 'Age', 'Location', 'CreditScore','DrivingExperience','AnnualMileage','Gender','MaritalStatus','RiskCategory']
+	
 
-#get pandas dataframe and convert it into numpy array
-datafr = pd.read_csv(url)
-array = np.array(datafr)
-# Split-out validation dataset
-X = array[:,0:9]
-Y = array[:,9]
-validation_size = 0.20
-seed = 7
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+	#get pandas dataframe and convert it into numpy array
+	datafr = pd.read_csv(url)
+	array = np.array(datafr)
+	# Split-out validation dataset
+	X = array[:,0:9]
+	Y = array[:,9]
+	validation_size = 0.00
+	seed = 7
+	X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
-# Test options and evaluation metric
-seed = 7
-scoring = 'accuracy'
+	
+	lda = LinearDiscriminantAnalysis()
+	lda.fit(X_train,Y_train)	
+	result = lda.predict(input)
+	print result
+	return result
 
-#Spot Check Algorithms
-models = []
-models.append(('LR', LogisticRegression()))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC()))
-# evaluate each model in turn
-results = []
-names = []
-for name, model in models:
-	kfold = model_selection.KFold(n_splits=10, random_state=seed)
-	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-	results.append(cv_results)
-	names.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-	print(msg)
