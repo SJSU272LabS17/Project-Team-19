d593bea08e7a17ad26fd4c6abdaca0c913e8126a
rovin284 <rovinsingh.patwal@sjsu.edu>
2017-05-14 20:26:58 -0700

    Updated file

--- FrontEnd/test.py
+++ FrontEnd/test.py
@@ -1,6 +1,8 @@
 
 
-def xyz(name,dob):
-    print name
-    print dob
+def xyz(age,PlateNo,Experience,Zip_Code,Mileage,Gender,Marital_Status):
+    print age
+    print PlateNo
+    print Gender
+    print Marital_Status
     return "Risky"
